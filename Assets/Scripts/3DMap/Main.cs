8c57b6eaf062cc9cd160904a5d9dd314eb354cf4
mikumeow <1287835568@qq.com>
2019-09-18 16:16:32 +0900

    Revert "Merge branch 'master' into countdownui"
    This reverts commit b86fe6bba16f9b7fbde14baa63462855ee6604be, reversing
    changes made to e7d8d8ae66f90d347f4627d0e730f19dad250a69.

--- Assets/Scripts/3DMap/Main.cs
+++ Assets/Scripts/3DMap/Main.cs
@@ -12,7 +12,7 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MapLoader.loadMap("Map_Grassland");
+        MapLoader.loadMap("Map1");
     }
 
     // Update is called once per frame
