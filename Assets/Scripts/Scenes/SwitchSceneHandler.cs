31346b68d45de640c50ce82d6c1fe727715e8210
mikumeow <1287835568@qq.com>
2019-09-21 03:39:07 +0900

    MadeBasicScenes
    Basic scenes built.
    Still working on scene transfer codes.

---
+++ Assets/Scripts/Scenes/SwitchSceneHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntersceneBehaviour : MonoBehaviour
+{
+    // Inter-Scene data
+    private static bool g_isHost;
+    private static string g_username;
+
+    public static bool G_isHost { get => g_isHost; set => g_isHost = value; }
+    public static string G_username { get => g_username; set => g_username = value; }
+}
