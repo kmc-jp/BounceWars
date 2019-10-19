b169869136eb88377ca22e28beaab883d19b4516
spinachpasta <spinatchpasta@gmail.com>
2019-10-19 11:30:37 +0900

    friction works 2

---
+++ Assets/Scripts/3DMap/MapPhysicsMaterial.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MapPhysicsMaterial", menuName = "Map/MapPhysicsMaterial", order = 1)]
+public class MapPhysicsMaterial : ScriptableObject
+{
+    public float friction;
+}
