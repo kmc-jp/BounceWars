12e605efd9e63a01be7b1bda9bc0ed334eca3b1a
tinaxd <31384595+tinaxd@users.noreply.github.com>
2019-11-15 15:10:15 +0900

    arrow rotation

---
+++ Assets/Scripts/Unit/NonPlayer/ArrowRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowRotation : MonoBehaviour
+{
+    private BasicUnit bu;
+
+    private void Awake()
+    {
+        bu = GetComponent<BasicUnit>();
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 vel = new Vector3(bu.unit.vx, 0, bu.unit.vz);
+        Vector3 relToCam = transform.position - Camera.main.transform.position;
+        transform.LookAt(transform.position + vel, relToCam);
+    }
+}
