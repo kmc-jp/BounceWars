fecb0c828e5be23c30f3e23b4d747c97c99cc1a8
spinachpasta <spinatchpasta@gmail.com>
2019-11-15 22:35:04 +0900

    target circle animation

---
+++ Assets/Scripts/Effect/KeepRotating.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepRotating : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Time.deltaTime*180, 0);
+    }
+}
