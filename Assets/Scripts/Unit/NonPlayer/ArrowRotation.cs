3878dec62ed6f52167d833ba320441fe9ab9e465
tinaxd <31384595+tinaxd@users.noreply.github.com>
2019-11-15 15:38:32 +0900

    Update ArrowRotation.cs
    use Unit instead of BasicUnit

--- Assets/Scripts/Unit/NonPlayer/ArrowRotation.cs
+++ Assets/Scripts/Unit/NonPlayer/ArrowRotation.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class ArrowRotation : MonoBehaviour
 {
-    private BasicUnit bu;
+    private Unit unit;
 
-    private void Awake()
+    private void Start()
     {
-        bu = GetComponent<BasicUnit>();
+        Simulator sim = GetComponent<UnitInfoTag>().sim;
+        unit = sim.GetUnit(GetComponent<UnitInfoTag>().uuid);
     }
 
     private void LateUpdate()
     {
-        Vector3 vel = new Vector3(bu.unit.vx, 0, bu.unit.vz);
+        if (unit == null)
+            return;
+        Vector3 vel = new Vector3(unit.vx, 0, unit.vz);
         Vector3 relToCam = transform.position - Camera.main.transform.position;
         transform.LookAt(transform.position + vel, relToCam);
     }
